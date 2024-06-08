47e0aba6b7bc98b6704e03c30113792369002e05
Matias Cardenas <cardenasmatias.1990@gmail.com>
2024-06-08 23:19:06 +0200

    feat: adding constants, models and migrations in core app

---
+++ marketing_op/core/constants.py
@@ -0,0 +1,19 @@
+CAMPAIGN_TYPES = [
+    ("branding", "BRANDING"),
+    ("performance", "PERFORMANCE"),
+    ("always on", "ALWAYS ON"),
+]
+
+CHANNELS = [
+    ("tv", "TV"),
+    ("radio", "RADIO"),
+    ("ooh", "OOH"),
+    ("facebook", "FACEBOOK"),
+    ("print", "PRINT"),
+    ("tiktok", "TIKTOK"),
+    ("instagram", "INSTAGRAM"),
+    ("db360 - prospecting", "DB360"),
+    ("db360 - retargeting", "DB360 - RETARGETING"),
+    ("google - branded search", "GOOGLE - BRANDED SEARCH"),
+    ("google - non-branded search", "GOOGLE - NON-BRANDED SEARCH"),
+]
--- marketing_op/core/models.py
+++ marketing_op/core/models.py
@@ -1,1 +1,43 @@
-# Create your models here.
+from django.db import models
+
+from core.constants import CAMPAIGN_TYPES, CHANNELS
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class CampaignType(models.Model):
+    name = models.CharField(max_length=50, choices=CAMPAIGN_TYPES, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=255, choices=CHANNELS, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Campaign(models.Model):
+    name = models.CharField(max_length=255)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    campaign_type = models.ForeignKey(CampaignType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Conversion(models.Model):
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    date = models.DateField()
+    conversions = models.FloatField()
+
+    def __str__(self):
+        return f"{self.campaign.name} - {self.channel.name} - {self.date}"
